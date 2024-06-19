127bc22e274e5a83de428125c1e571502497d900
Luan1511 <pluan7911@gmail.com>
2024-06-19 13:32:37 +0700

    update 19/06

---
+++ public/JS/about.js
@@ -0,0 +1,30 @@
+const menu = document.querySelector("#menu")
+const menuBtn = document.querySelector("#menu-btn")
+const hero_img_1 = document.querySelector("#hero_img_1")
+const cursor = document.querySelector('.cursor_cus')
+
+menuBtn.addEventListener('click',()=>{
+    menu.classList.toggle('invisible');
+    document.querySelector('.menuLogo').classList.toggle('hidden');
+    document.querySelector('.header').classList.toggle('hidden');
+})
+
+// Scrolled
+const navbar = document.querySelector('.navbar')
+const logo = document.querySelector('.logo_container')
+
+window.addEventListener('scroll', () => {
+    let scrollPosition = window.scrollY;
+
+    if (scrollPosition > 50) {
+        logo.style.visibility = 'visible';
+    } else {
+        logo.style.visibility = 'hidden';
+    }
+
+    if (scrollPosition > 50) {
+        navbar.style.top = '0';
+    } else {
+        navbar.style.top = 'auto';
+    }
+});
--- query.md
+++ query.md
@@ -1,2 +1,3 @@
 - Start Tailwind: 
 > npx tailwindcss -i ./src/tailwind.css -o ./public/css/style.css --watch
+https://github.com/HoanghoDev/youtube_v2/tree/main/slider_3d
